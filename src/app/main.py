26ce4d81209494ef80ab956ccda149d5f5c8d505
Andrew Dymond <andy.dymond@gmail.com>
2020-03-17 15:15:53 -0600

    Spin up & connect postgres

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+
 from app.api import ping
+from app.db import engine, metadata, database
+
+metadata.create_all(engine)
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.onvent("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 app.include_router(ping.router)
